0485b2ff4ade26835ac7885f9e7c703774a97344
Davide Guerrazzi <g.davide2005@gmail.com>
2022-09-09 14:43:42 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -18,4 +18,8 @@ import setuptools
 with open('requirements.txt', 'r') as f:
     install_requires = f.read().splitlines()
 
-setuptools.setup(name='may_browse', packages=['may_browse'], install_requires=install_requires)
+setuptools.setup(
+    name='may_browse', 
+    packages=['may_browse'], 
+    install_requires=install_requires
+)
